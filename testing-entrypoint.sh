92c60411a2d2faeb1d7e28501535b9dc5b1dcd2e
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2020-03-06 11:09:55 -0600

    added comments

--- testing-entrypoint.sh
+++ testing-entrypoint.sh
@@ -7,6 +7,8 @@ while [ $(nc -z db 3306 ; echo $?) != 0 ] ; do
  sleep 2
 done
 set -x
+#python manage.py migrate --fake # Reset the migrations for the "built-in" apps
+#python manage.py makemigrations webapp
 python manage.py migrate 
 ./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" 
 python manage.py runserver 0.0.0.0:80
