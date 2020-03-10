f7539cbe353019a39ab6e9fff1423f887f3c7690
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2020-03-10 16:07:36 -0500

    increase user field size

--- app/webapp/webapp/models.py
+++ app/webapp/webapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Token(models.Model):
-    user = models.CharField(max_length=20)
+    user = models.CharField(max_length=50)
     tokenValue = models.CharField(max_length=20)
     expires = models.DateTimeField()
     scope = models.TextField(default='default')
