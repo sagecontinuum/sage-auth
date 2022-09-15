cdfe2eec5a06929c7c79f29f1dfe48c840e3a555
nconrad <1194246+nconrad@users.noreply.github.com>
2022-09-15 10:40:42 -0500

    add read-only test

--- app/webapp/webapp/test_user_api.py
+++ app/webapp/webapp/test_user_api.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from webapp.models import Profile
 from .views import util_create_sage_token
 import json
 
@@ -25,7 +26,6 @@ class UserProfileTests(TestCase):
         r = self.client.put('/user_profile/test', data, HTTP_AUTHORIZATION='Sage some_foo', content_type='application/json')
         self.assertEqual(r.status_code, 403)
 
-    def testUserProfileBadBearer(self):
         r = self.client.get('/user_profile/test', HTTP_AUTHORIZATION='Foo some_foo')
         self.assertEqual(r.status_code, 403)
 
@@ -51,8 +51,8 @@ class UserProfileTests(TestCase):
         self.assertEqual(r.status_code, 200)
 
         u = json.loads(r.content)
-        self.assertEqual(u['bio'], 'a new bio')
-        self.assertEqual(u['department'], 'new department')
+        self.assertEqual(u['bio'], data['bio'])
+        self.assertEqual(u['department'],  data['department'])
         self.assertEqual(u['organization'], 'an org')
 
         # check db
@@ -60,9 +60,37 @@ class UserProfileTests(TestCase):
         self.assertEqual(r.status_code, 200)
 
         u = json.loads(r.content)
-        self.assertEqual(u['bio'], 'a new bio')
-        self.assertEqual(u['department'], 'new department')
+        self.assertEqual(u['bio'], data['bio'])
+        self.assertEqual(u['department'],  data['department'])
         self.assertEqual(u['organization'], 'an org')
+
+    def testUserProfileReadOnly(self):
+        token = self.setUpUserAndToken('test')
+        authorization = f'Sage {token}'
+
+        # sage_username is immutable
+        data = {'sage_username': 'foo_user'}
+        r = self.client.put('/user_profile/test', data, HTTP_AUTHORIZATION=authorization, content_type='application/json')
+        self.assertEqual(r.status_code, 200)
+
+        u = json.loads(r.content)
+        self.assertEqual(u['sage_username'], 'test')
+
+        # django user connection is immutable
+        data = {'user': 123, 'user_id': 123, 'id': 123}
+        r = self.client.put('/user_profile/test', data, HTTP_AUTHORIZATION=authorization, content_type='application/json')
+        self.assertEqual(r.status_code, 200)
+
+        # response
+        u = json.loads(r.content)
+        self.assertEqual(u['user'], 1)
+        self.assertEqual(u['id'], 1)
+
+        # verify db
+        user = Profile.objects.get(user=1)
+        self.assertEqual(user.sage_username, 'test')
+        self.assertEqual(user.user.username, 'test')
+        self.assertEqual(user.id, 1)
 
     def setUpUserAndToken(self, username):
         '''create a user with an empty profile and random token'''
